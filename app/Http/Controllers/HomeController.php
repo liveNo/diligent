b521871bf6d25425e0d8ab3ecefca750a68509f0
xululu <xuyanlu0424@163.com>
2016-02-23 12:41:15 +0800

    add home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Validator;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller 
+{
+
+    public function index() 
+    {
+    	echo "Welcome to Laravel";die;
+
+    }
+}
