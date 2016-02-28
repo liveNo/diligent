d559e726116e87228a3ef7d26b4a1bf2e8cebdd5
xululu <xuyanlu0424@163.com>
2016-02-28 22:20:54 +0800

    move css to public

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 		<title>Foundation | Welcome</title>
-		<!--<link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">-->
-		<link rel="stylesheet" href="{{ URL::asset('/css/foundation.min.css') }}">
+		<link rel="stylesheet" href="http://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.min.css">
+		<!--<link rel="stylesheet" href="{{ URL::asset('/') }}/css/foundation.min.css">-->
 
 	</head>
 	<body>
